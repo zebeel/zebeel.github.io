412ebb19367830eb46302221d59725f376b506ac
Lee Vaz <chuong.le.jp@gmail.com>
2020-11-17 18:19:43 +0900

    commentable

--- post/my-lil-sis-wedding/js/main.js
+++ post/my-lil-sis-wedding/js/main.js
@@ -230,3 +230,92 @@
 
 
 }());
+
+$(document).ready(function() {
+	$.ajax({
+		url: ' http://34.125.87.195/simple-comment-api/public/api/all-comments?to=khanh-tram',
+		method: 'GET',
+		success: function (data) {
+			$('.wish-count').text(data.count)
+			if(data.count) {
+				for(let comment of data.comments) {
+					fillComments(comment, 'after', 'hide-comment')
+				}
+			}
+			loadComment()
+		}
+	})
+
+	function fillComments(comment, pos = 'after', addClass = '') {
+		let html = '				<div class="be-comment comment-line '+addClass+'">\n' +
+			'                            <div class="be-img-comment">\n' +
+			'                                <img src="'+comment.avatar+'" class="be-ava-comment">\n' +
+			'                            </div>\n' +
+			'                            <div class="be-comment-content">\n' +
+			'                                <span class="be-comment-name">'+comment.by+'</span>\n' +
+			'                                <span class="be-comment-time"><i class="fa fa-clock"></i> '+comment.clock+'</span>\n' +
+			'                                <p class="be-comment-text">'+comment.content+'</p>\n' +
+			'                            </div>\n' +
+			'                        </div>'
+
+		if (pos === 'after')
+			$('.comment-lines').append(html)
+		else
+			$('.comment-lines').prepend(html)
+	}
+
+	$('#more-comments').click(function (e) {
+		loadComment()
+	})
+
+	function loadComment() {
+		let lines = $('.comment-lines .comment-line')
+		let load_count = 0, load_max = 10
+		for(let i = 0; i < lines.length; i++) {
+			let line = lines[i]
+			if(load_count < load_max && line.classList.contains('hide-comment')) {
+				line.classList.remove('hide-comment')
+				load_count++
+			}
+		}
+		if(load_count < load_max) {
+			$('#more-comments').addClass("disabled")
+		}
+	}
+
+	$('#send-comment').click(function (e) {
+		if($('#send-comment').hasClass('disabled')){
+			return
+		}
+		if(!$('#wish-content').val()) {
+			$('#wish-content').focus()
+			return
+		}
+		if(!$('#wish-by').val()) {
+			$('#wish-by').focus()
+			return
+		}
+		$.ajax({
+			url: 'http://34.125.87.195/simple-comment-api/public/api/new-comment',
+			method: 'POST',
+			data: {by: $('#wish-by').val(), to: 'khanh-tram', content: $('#wish-content').val()},
+			success: function (data) {
+				if (data.error) {
+					$.alert({
+						title: 'Alert!',
+						content: 'Vui long thử lại lần nữa!',
+					});
+					return
+				}
+				fillComments(data.comment, 'before')
+				$('#wish-by').val('')
+				$('#wish-content').val('')
+				$('.wish-count').text(parseInt($('.wish-count').text()) + 1)
+				$('#send-comment').addClass('disabled')
+			},
+			error: function (e) {
+				console.log(e)
+			}
+		})
+	})
+})
